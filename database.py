87a820c5377eba553ed3705061173d7ab42a1fd5
Dark-bean <zenthon14@gmail.com>
2023-07-24 23:10:25 -0600

    functional website done, still need to add finishing touches

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, insert
 from dotenv import load_dotenv
 import os
 
@@ -40,3 +40,15 @@ def load_job_from_db(id):
       return None
     else:
       return dict(jobs[0])
+    
+
+
+def add_application_to_db(job, data):
+  print(data)
+  with engine.connect() as conn:
+
+    stm = text("INSERT INTO applications (job_id, full_name, email, education,linkedin_url, work_experience, resume_url) VALUES (:job_id,:full_name,:email, :education, :linkedin_url, :work_experience, :resume_url)").\
+    bindparams(job_id=job, full_name=data["full_name"], email=data["email"], education=data['education'], work_experience=data['work_experience'], resume_url=data['resume_url'], linkedin_url=data['linkedin_url'])
+    conn.execute(stm)
+
+    
